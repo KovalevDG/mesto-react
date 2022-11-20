dbb663dcbec76f99bb5cf7901357bc0add8cd744
dmitry <kronalf@inbox.ru>
2022-11-20 20:31:07 +0300

    project refactoring in react on check #4

--- mesto/src/components/Card.js
+++ mesto/src/components/Card.js
@@ -9,10 +9,10 @@ class Card extends React.Component{
    render() {
       return (
          <article className="element">
-            <div className="element__image" alt="#" style={{ backgroundImage: `url(${this.props.card.link})` }} onClick={this.handleClick} />
+            <div className="element__image" style={{ backgroundImage: `url(${this.props.card.link})` }} onClick={this.handleClick} />
             <button className="element__delete" type="button"></button>
             <div className="element__description">
-               <h2 className="element__text">{this.props.name}</h2>
+               <h2 className="element__text">{this.props.card.name}</h2>
                <div className="element__like">
                   <button className="element__like-icon" type="button"></button>
                   <span className="element__like-counter">{this.props.card.likes.length}</span>
